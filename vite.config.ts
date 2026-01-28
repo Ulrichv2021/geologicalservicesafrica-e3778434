0eea2029584b524d768927fa703ef40dfb9b77b9
Ulrichv2021 <91911680+Ulrichv2021@users.noreply.github.com>
2026-01-28 07:19:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc"; // Keep your swc plugin
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADD THIS LINE BELOW
+  base: '/', 
+  
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
